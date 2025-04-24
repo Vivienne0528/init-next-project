e488216c73237d0afc49f598bcbad91099dc5c79
YuweiGu98 <vivienne0528.gu@gmail.com>
2025-04-24 14:49:35 +1000

    project setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
+import { store } from "@/store/store";
 import "@/styles/globals.css";
+import { Provider as ChakraProvider } from "@/components/ui/provider"
 import type { AppProps } from "next/app";
+import { Provider as ReduxProvider } from "react-redux";
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ReduxProvider store={store}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </ReduxProvider>
+  );
 }
